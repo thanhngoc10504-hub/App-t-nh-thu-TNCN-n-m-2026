2f235d002542b39a517c277744ef41921e1ad83c
thanhngoc10504-hub <thanhngoc10504@gmail.com>
2026-06-20 17:54:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ import streamlit as st
 st.set_page_config(page_title="App Tính Thuế TNCN Việt Nam 2026", page_icon="💰", layout="centered")
 
 # --- CHÈN LOGO THEO FILE TRỰC TIẾP ---
-st.image("logo.jpg")
+st.image("logo.jpg.jfif")
 
 # --- THÔNG TIN THÀNH VIÊN VÀ ĐỀ TÀI ---
-st.markdown("### 📝 **TS. VŨ ĐỨC BÌNH**")
+st.markdown("### 📝 **Nguyễn Thanh Ngọc**")
 
 st.title("💰 Ứng Dụng Tính Thuế Thu Nhập Cá Nhân")
 st.write("Cập nhật đầy đủ Lương, Thưởng, Tăng ca, Phụ cấp theo luật thuế mới nhất năm 2026")
